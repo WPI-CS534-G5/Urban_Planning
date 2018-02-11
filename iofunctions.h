83d1607cd412fb0b1cbfb0b65c8eb1ebf9d2e4bf
nkalampalikis@github.com <nkalampalikis97@gmail.com>
2018-02-11 18:39:09 -0500

    I/O functions

--- iofunctions.h
+++ iofunctions.h
@@ -1,3 +1,14 @@
 
-
+#ifndef IOFUNCTIONS_H
+#define IOFUNCTIONS_H
+#include <vector>
+#include <string>
 #include "global.h"
+
+using std::vector;
+using std::string;
+
+int printMap(vector<vector<int>> map);
+int readMap(struct Params &params);
+
+#endif  //IOFUNCTIONS.H
